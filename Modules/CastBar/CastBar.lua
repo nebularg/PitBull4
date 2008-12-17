a210826bf9b88743f50f7853f7326118c5152d21
Cameron Knight <ckknight@ckknight-laptop.local>
2008-12-16 22:43:37 -0600

    simplify some logic.

--- Modules/CastBar/CastBar.lua
+++ Modules/CastBar/CastBar.lua
@@ -43,7 +43,7 @@ function PitBull4_CastBar.GetValue(frame)
 		return (GetTime() - startTime) / (data.endTime - startTime)
 	elseif data.channeling then	
 		local endTime = data.endTime
-		castBar:SetValue((endTime - GetTime()) / (endTime - data.startTime))
+		return (endTime - GetTime()) / (endTime - data.startTime)
 	elseif data.fadeOut then
 		return frame.CastBar:GetValue()
 	end
@@ -87,147 +87,42 @@ end
 PitBull4_CastBar:SetValueFunction('GetValue')
 PitBull4_CastBar:SetColorFunction('GetColor')
 
-function PitBull4_CastBar.UNIT_SPELLCAST_SENT(event, unit, spell, rank, target)
+local function updateInfo(_, unit)
 	local guid = UnitGUID(unit)
-	local found = false
-	for frame in PitBull4.IterateFramesForGUID(guid) do
-		if frame.CastBar then
-			found = true
-			break
-		end
-	end
-	if not found then
-		return
-	end
-	
 	local data = castData[guid]
 	if not data then
 		data = new()
 		castData[guid] = data
 	end
-
-	if target == "" then
-		target = nil
-	end
-	data.target = target
-end
-
-function PitBull4_CastBar.UNIT_SPELLCAST_START(event, unit)
-	local guid = UnitGUID(unit)
-	local found = false
-	for frame in PitBull4.IterateFramesForGUID(guid) do
-		if frame.CastBar then
-			found = true
-			break
-		end
-	end
-	if not found then
-		return
-	end
-
-	local data = castData[guid]
-	if not data then
-		data = new()
-		castData[guid] = data
-	end	
-
-	local spell, rank, displayName, icon, startTime, endTime = UnitCastingInfo(unit)
-	data.spell = spell
-	data.rank = rank
-	data.displayName = displayName
-	data.icon = icon
-	data.startTime = startTime * 0.001
-	data.endTime = endTime * 0.001
-	local channeling = event == "UNIT_SPELLCAST_CHANNEL_START"
-	data.casting = not channeling
-	data.channeling = channeling
-	data.fadeOut = nil
-end
-
-function PitBull4_CastBar.UNIT_SPELLCAST_STOP(event, unit)
-	local guid = UnitGUID(unit)
-	local data = castData[guid]
-	if not data or not data.casting then
-		return
-	end
-	data.casting = nil
-	data.fadeOut = 1
-	data.stopTime = GetTime()
-end
-
-function PitBull4_CastBar.UNIT_SPELLCAST_FAILED(event, unit)
-	local guid = UnitGUID(unit)
-	local data = castData[guid]
-	if not data or data.fadeOut then
-		return
-	end
-	data.casting = nil
-	data.channeling = nil
-	data.fadeOut = 1
-	data.stopTime = GetTime()
-end
-
-function PitBull4_CastBar.UNIT_SPELLCAST_INTERRUPTED(event, unit)
-	local guid = UnitGUID(unit)
-	local data = castData[guid]
-	if not data then
-		return
-	end
-	data.casting = nil
-	data.channeling = nil
-	data.fadeOut = 1
-	data.stopTime = GetTime()
-end
-
-function PitBull4_CastBar.UNIT_SPELLCAST_DELAYED(event, unit)
-	local guid = UnitGUID(unit)
-	local data = castData[guid]
-	if not data or not data.casting then
-		return
-	end
 	
 	local spell, rank, displayName, icon, startTime, endTime = UnitCastingInfo(unit)
-
-	if not spell or not startTime or not endTime then
-		return
-	end
-
-	local oldStart = data.startTime
-
-	data.startTime = startTime * 0.001
-	data.endTime = endTime * 0.001
-end
-
-function PitBull4_CastBar.UNIT_SPELLCAST_CHANNEL_UPDATE(event, unit)
-	local guid = UnitGUID(unit)
-	local data = castData[guid]
-	if not data then
-		return
-	end
-
-	local spell, rank, displayName, icon, startTime, endTime = UnitChannelInfo(unit)
-
+	local channeling = false
 	if not spell then
+		spell, rank, displayName, icon, startTime, endTime = UnitChannelInfo(unit)
+		channeling = true
+	end
+	if spell then
+		data.icon = icon
+		data.startTime = startTime * 0.001
+		data.endTime = endTime * 0.001
+		data.casting = not channeling
+		data.channeling = channeling
+		data.fadeOut = false
+		data.stopTime = nil
+		return
+	end
+	
+	if not data.icon then
 		castData[guid] = del(data)
 		return
 	end
-
-	local oldStart = data.startTime
-	data.startTime = startTime * 0.001
-	data.endTime = endTime * 0.001
-end
-
-function PitBull4_CastBar.UNIT_SPELLCAST_CHANNEL_STOP(event, unit)
-	local guid = UnitGUID(unit)
-	local data = castData[guid]
-	if not data then
-		return
-	end
 	
-	data.channeling = nil
-	data.casting = nil
-	data.fadeOut = 1
-	data.stopTime = GetTime()
+	data.casting = false
+	data.channeling = false
+	data.fadeOut = true
+	if not data.stopTime then
+		data.stopTime = GetTime()
+	end
 end
 
 local playerGUID = UnitGUID("player")
@@ -242,14 +137,14 @@ function PitBull4_CastBar.FixCastData()
 				found = true
 				if data.casting then
 					if currentTime > data.endTime and playerGUID ~= guid then
-						data.casting = nil
-						data.fadeOut = 1
+						data.casting = false
+						data.fadeOut = true
 						data.stopTime = currentTime
 					end
 				elseif data.channeling then
 					if currentTime > data.endTime then
-						data.channeling = nil
-						data.fadeOut = 1
+						data.channeling = false
+						data.fadeOut = true
 						data.stopTime = currentTime
 					end
 				elseif data.fadeOut then
@@ -284,31 +179,7 @@ function PitBull4_CastBar.OnUpdate(frame)
 		return
 	end
 	
-	if select(3, UnitCastingInfo(unit)) then
-		PitBull4_CastBar.UNIT_SPELLCAST_START("UNIT_SPELLCAST_START", unit)
-		return
-	end
-	
-	if select(3, UnitChannelInfo(unit)) then
-		PitBull4_CastBar.UNIT_SPELLCAST_START("UNIT_SPELLCAST_CHANNEL_START", unit)
-		return
-	end
-	
-	local guid = UnitGUID(unit)
-	local data = castData[guid]
-	if not data then
-		return
-	end
-
-	if data.channeling then
-		PitBull4_CastBar.UNIT_SPELLCAST_CHANNEL_STOP("UNIT_SPELLCAST_CHANNEL_STOP", unit)
-		return
-	end
-	
-	if data.casting then
-		PitBull4_CastBar.UNIT_SPELLCAST_STOP("UNIT_SPELLCAST_STOP", unit)
-		return
-	end
+	updateInfo(nil, unit)
 end
 
 PitBull4.Utils.AddTimer(function()
@@ -316,12 +187,11 @@ PitBull4.Utils.AddTimer(function()
 	PitBull4_CastBar:UpdateAll()
 end)
 PitBull4_CastBar:AddFrameScriptHook("OnUpdate", PitBull4_CastBar.OnUpdate)
-PitBull4.Utils.AddEventListener("UNIT_SPELLCAST_SENT", PitBull4_CastBar.UNIT_SPELLCAST_SENT)
-PitBull4.Utils.AddEventListener("UNIT_SPELLCAST_START", PitBull4_CastBar.UNIT_SPELLCAST_START)
-PitBull4.Utils.AddEventListener("UNIT_SPELLCAST_CHANNEL_START", PitBull4_CastBar.UNIT_SPELLCAST_START)
-PitBull4.Utils.AddEventListener("UNIT_SPELLCAST_STOP", PitBull4_CastBar.UNIT_SPELLCAST_STOP)
-PitBull4.Utils.AddEventListener("UNIT_SPELLCAST_FAILED", PitBull4_CastBar.UNIT_SPELLCAST_FAILED)
-PitBull4.Utils.AddEventListener("UNIT_SPELLCAST_INTERRUPTED", PitBull4_CastBar.UNIT_SPELLCAST_INTERRUPTED)
-PitBull4.Utils.AddEventListener("UNIT_SPELLCAST_DELAYED", PitBull4_CastBar.UNIT_SPELLCAST_DELAYED)
-PitBull4.Utils.AddEventListener("UNIT_SPELLCAST_CHANNEL_UPDATE", PitBull4_CastBar.UNIT_SPELLCAST_CHANNEL_UPDATE)
-PitBull4.Utils.AddEventListener("UNIT_SPELLCAST_CHANNEL_STOP", PitBull4_CastBar.UNIT_SPELLCAST_CHANNEL_STOP)
+PitBull4.Utils.AddEventListener("UNIT_SPELLCAST_START", updateInfo)
+PitBull4.Utils.AddEventListener("UNIT_SPELLCAST_CHANNEL_START", updateInfo)
+PitBull4.Utils.AddEventListener("UNIT_SPELLCAST_STOP", updateInfo)
+PitBull4.Utils.AddEventListener("UNIT_SPELLCAST_FAILED", updateInfo)
+PitBull4.Utils.AddEventListener("UNIT_SPELLCAST_INTERRUPTED", updateInfo)
+PitBull4.Utils.AddEventListener("UNIT_SPELLCAST_DELAYED", updateInfo)
+PitBull4.Utils.AddEventListener("UNIT_SPELLCAST_CHANNEL_UPDATE", updateInfo)
+PitBull4.Utils.AddEventListener("UNIT_SPELLCAST_CHANNEL_STOP", updateInfo)
