3c8795a083be8fa6fb58112865b5926b6dc9b6cc
Cameron Knight <ckknight@ckknight-laptop.local>
2009-01-10 15:29:01 -0600

    mess with some locale stuff

--- Localization/zhTW.lua
+++ Localization/zhTW.lua
@@ -2,5 +2,5 @@ local L = LibStub("AceLocale-3.0"):NewLocale("PitBull4", "zhTW")
 if not L then return end
 
 --[===[@non-debug@
-@localization(locale="zhTW", format="lua_additive_table", table_name="L")@
+@localization(locale="zhTW", format="lua_additive_table")@
 --@end-non-debug@]===]
