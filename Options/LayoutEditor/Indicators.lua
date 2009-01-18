e9ede80a3f4fe318e6305de0bb9e6a74574e3523
Cameron Knight <ckknight@ckknight-laptop.local>
2009-01-18 04:05:33 -0600

    whoops, fix some references

--- Options/LayoutEditor/Indicators.lua
+++ Options/LayoutEditor/Indicators.lua
@@ -278,7 +278,7 @@ function PitBull4.Options.get_layout_editor_indicator_options()
 			for id, module in PitBull4:IterateModulesOfType("status_bar_provider") do
 				local db = GetLayoutDB(module)
 				if db.enabled then
-					for name in pairs(db.bars) do
+					for name in pairs(db.elements) do
 						t[id .. ";" .. name] = ("%s: %s"):format(module.name, name)
 					end
 				end
