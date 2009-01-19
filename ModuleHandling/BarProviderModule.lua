267cd3b0263254b16e9320e0f290e54bdc26a80e
Cameron Knight <ckknight@ckknight-laptop.local>
2009-01-19 17:39:07 -0600

    make the :Call* methods for bar provider modules private.

--- ModuleHandling/BarProviderModule.lua
+++ ModuleHandling/BarProviderModule.lua
@@ -26,6 +26,108 @@ if not LibSharedMedia then
 	LibSharedMedia = LibStub("LibSharedMedia-3.0", true)
 end
 
+--- Call the :GetValue function on the status bar module regarding the given frame.
+-- @param self the module
+-- @param frame the frame to get the value of
+-- @param bar_db the layout db for the specific bar
+-- @usage local value, extra = call_value_function(MyModule, someFrame)
+-- @return nil or a number within [0, 1]
+-- @return nil or a number within (0, 1 - value]
+local function call_value_function(self, frame, bar_db)
+	if not self.GetValue then
+		return nil, nil
+	end
+	local value, extra
+	if frame.guid then
+		value, extra = self:GetValue(frame, bar_db)
+	end
+	
+	if not value and PitBull4.config_mode and self.GetExampleValue then
+		value, extra = self:GetExampleValue(frame, bar_db)
+	end
+	if not value then
+		return nil, nil
+	end
+	if value < 0 or value ~= value then -- NaN
+		value = 0
+	elseif value > 1 then
+		value = 1
+	end
+	if not extra or extra <= 0 or extra ~= extra then -- NaN
+		return value, nil
+	end
+	
+	local max = 1 - value
+	if extra > max then
+		extra = max
+	end
+	
+	return value, extra
+end
+
+--- Call the :GetColor function on the status bar module regarding the given frame.
+--- Call the color function which the current status bar module has registered regarding the given frame.
+-- @param self the module
+-- @param frame the frame to get the color of
+-- @param bar_db the layout db for the specific bar
+-- @param value the value as returned by :call_value_function
+-- @param extra the extra value as returned by :call_value_function
+-- @usage local r, g, b, a = call_color_function(MyModule, someFrame)
+-- @return red value within [0, 1]
+-- @return green value within [0, 1]
+-- @return blue value within [0, 1]
+-- @return alpha value within [0, 1]
+local function call_color_function(self, frame, bar_db, value, extra)
+	local custom_color = bar_db.custom_color
+	if custom_color then
+		return unpack(custom_color)
+	end
+	
+	if not self.GetColor then
+		return 0.7, 0.7, 0.7, 1
+	end
+	local r, g, b, a = self:GetColor(frame, bar_db, value, extra)
+	if (not r or not g or not b) and PitBull4.config_mode and self.GetExampleColor then
+		r, g, b, a = self:GetExampleColor(frame, bar_db, value, extra)
+	end
+	if not r or not g or not b then
+		return 0.7, 0.7, 0.7, a or 1
+	end
+	return r, g, b, a or 1
+end
+
+--- Call the :GetExtraColor function on the status bar module regarding the given frame.
+--- Call the color function which the current status bar module has registered regarding the given frame.
+-- @param self the module
+-- @param frame the frame to get the color of
+-- @param bar_db the layout db for the specific bar
+-- @param value the value as returned by :call_value_function
+-- @param extra the extra value as returned by :call_value_function
+-- @usage local r, g, b, a = call_extra_color_function(MyModule, someFrame)
+-- @return red value within [0, 1]
+-- @return green value within [0, 1]
+-- @return blue value within [0, 1]
+-- @return alpha value within [0, 1] or nil
+local function call_extra_color_function(self, frame, bar_db, value, extra)
+	local custom_color = bar_db.custom_color
+	if custom_color then
+		local r, g, b, a = custom_color
+		return (1 + 2*r) / 3, (1 + 2*g) / 3, (1 + 2*b) / 3, a
+	end
+	
+	if not self.GetExtraColor then
+		return 0.5, 0.5, 0.5, nil
+	end
+	local r, g, b, a = self:GetExtraColor(frame, value, extra)
+	if (not r or not g or not b) and PitBull4.config_mode and self.GetExampleExtraColor then
+		r, g, b, a = self:GetExampleExtraColor(frame, value, extra)
+	end
+	if not r or not g or not b then
+		return 0.5, 0.5, 0.5, nil
+	end
+	return r, g, b, a
+end
+
 --- Clear the status bar for the current module if it exists.
 -- @param frame the Unit Frame to clear
 -- @usage local update_layout = MyModule:ClearFrame(frame)
@@ -87,7 +189,7 @@ function BarProviderModule:UpdateFrame(frame)
 	for name, bar_db in pairs(layout_db.elements) do
 		local bar = bars[name]
 		
-		local value, extra = self:CallValueFunction(frame, bar_db)
+		local value, extra = call_value_function(self, frame, bar_db)
 		if not value then
 			if bar then
 				bar.db = nil
@@ -111,14 +213,14 @@ function BarProviderModule:UpdateFrame(frame)
 			bar:SetTexture(texture or [[Interface\TargetingFrame\UI-StatusBar]])
 			bar:SetValue(value)
 			
-			local r, g, b, a = self:CallColorFunction(frame, bar_db, value, extra or 0)
+			local r, g, b, a = call_color_function(self, frame, bar_db, value, extra or 0)
 			bar:SetColor(r, g, b)
 			bar:SetAlpha(a)
 			
 			if extra then
 				bar:SetExtraValue(extra)
 
-				local r, g, b, a = self:CallExtraColorFunction(frame, bar_db, value, extra)
+				local r, g, b, a = call_extra_color_function(self, frame, bar_db, value, extra)
 				bar:SetExtraColor(r, g, b)
 				bar:SetExtraAlpha(a)
 			else
@@ -133,103 +235,4 @@ function BarProviderModule:UpdateFrame(frame)
 	end
 	
 	return changed
-end
-
---- Call the :GetValue function on the status bar module regarding the given frame.
--- @param frame the frame to get the value of
--- @param bar_db the layout db for the specific bar
--- @usage local value, extra = MyModule:CallValueFunction(someFrame)
--- @return nil or a number within [0, 1]
--- @return nil or a number within (0, 1 - value]
-function BarProviderModule:CallValueFunction(frame, bar_db)
-	if not self.GetValue then
-		return nil, nil
-	end
-	local value, extra
-	if frame.guid then
-		value, extra = self:GetValue(frame, bar_db)
-	end
-	
-	if not value and PitBull4.config_mode and self.GetExampleValue then
-		value, extra = self:GetExampleValue(frame, bar_db)
-	end
-	if not value then
-		return nil, nil
-	end
-	if value < 0 or value ~= value then -- NaN
-		value = 0
-	elseif value > 1 then
-		value = 1
-	end
-	if not extra or extra <= 0 or extra ~= extra then -- NaN
-		return value, nil
-	end
-	
-	local max = 1 - value
-	if extra > max then
-		extra = max
-	end
-	
-	return value, extra
-end
-
---- Call the :GetColor function on the status bar module regarding the given frame.
---- Call the color function which the current status bar module has registered regarding the given frame.
--- @param frame the frame to get the color of
--- @param bar_db the layout db for the specific bar
--- @param value the value as returned by :CallValueFunction
--- @param extra the extra value as returned by :CallValueFunction
--- @usage local r, g, b, a = MyModule:CallColorFunction(someFrame)
--- @return red value within [0, 1]
--- @return green value within [0, 1]
--- @return blue value within [0, 1]
--- @return alpha value within [0, 1]
-function BarProviderModule:CallColorFunction(frame, bar_db, value, extra)
-	local custom_color = bar_db.custom_color
-	if custom_color then
-		return unpack(custom_color)
-	end
-	
-	if not self.GetColor then
-		return 0.7, 0.7, 0.7, 1
-	end
-	local r, g, b, a = self:GetColor(frame, bar_db, value, extra)
-	if (not r or not g or not b) and PitBull4.config_mode and self.GetExampleColor then
-		r, g, b, a = self:GetExampleColor(frame, bar_db, value, extra)
-	end
-	if not r or not g or not b then
-		return 0.7, 0.7, 0.7, a or 1
-	end
-	return r, g, b, a or 1
-end
-
---- Call the :GetExtraColor function on the status bar module regarding the given frame.
---- Call the color function which the current status bar module has registered regarding the given frame.
--- @param frame the frame to get the color of
--- @param bar_db the layout db for the specific bar
--- @param value the value as returned by :CallValueFunction
--- @param extra the extra value as returned by :CallValueFunction
--- @usage local r, g, b, a = MyModule:CallColorFunction(someFrame)
--- @return red value within [0, 1]
--- @return green value within [0, 1]
--- @return blue value within [0, 1]
--- @return alpha value within [0, 1] or nil
-function BarProviderModule:CallExtraColorFunction(frame, bar_db, value, extra)
-	local custom_color = bar_db.custom_color
-	if custom_color then
-		local r, g, b, a = custom_color
-		return (1 + 2*r) / 3, (1 + 2*g) / 3, (1 + 2*b) / 3, a
-	end
-	
-	if not self.GetExtraColor then
-		return 0.5, 0.5, 0.5, nil
-	end
-	local r, g, b, a = self:GetExtraColor(frame, value, extra)
-	if (not r or not g or not b) and PitBull4.config_mode and self.GetExampleExtraColor then
-		r, g, b, a = self:GetExampleExtraColor(frame, value, extra)
-	end
-	if not r or not g or not b then
-		return 0.5, 0.5, 0.5, nil
-	end
-	return r, g, b, a
 end
