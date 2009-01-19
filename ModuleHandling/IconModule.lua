310c0ddc158c6ed86fc1d5232cf13d0f6991c6bf
Cameron Knight <ckknight@ckknight-laptop.local>
2009-01-19 17:33:35 -0600

    make the :Call* methods for icon modules private.

--- ModuleHandling/IconModule.lua
+++ ModuleHandling/IconModule.lua
@@ -12,6 +12,53 @@ local IconModule = PitBull4:NewModuleType("icon", {
 	enabled = true,
 })
 
+--- Call the :GetTexture function on the icon module regarding the given frame.
+-- @param self the module
+-- @param frame the frame to get the texture of
+-- @usage local tex, c1, c2, c3, c4 = call_texture_function(MyModule, someFrame)
+-- @return texture the path to the texture to show
+local function call_texture_function(self, frame)
+	if not self.GetTexture then
+		-- no function, let's just return
+		return nil
+	end
+	local tex
+	if frame.guid then
+		tex = self:GetTexture(frame)
+	end
+	if not tex and PitBull4.config_mode and self.GetExampleTexture then
+		tex = self:GetExampleTexture(frame)
+	end
+	if not tex then
+		return nil
+	end
+	
+	return tex
+end
+
+--- Call the :GetTexCoord function on the icon module regarding the given frame.
+-- @param frame the frame to get the TexCoord of
+-- @param texture the texture as returned by call_texture_function
+-- @usage local c1, c2, c3, c4 = call_tex_coord_function(MyModule, someFrame, "SomeTexture")
+-- @return left TexCoord for left within [0, 1]
+-- @return right TexCoord for right within [0, 1]
+-- @return top TexCoord for top within [0, 1]
+-- @return bottom TexCoord for bottom within [0, 1]
+local function call_tex_coord_function(self, frame, texture)
+	if not self.GetTexCoord then
+		-- no function, let's just return the defaults
+		return 0, 1, 0, 1
+	end
+	local c1, c2, c3, c4 = self:GetTexCoord(frame, texture)
+	if not c4 and PitBull4.config_mode and self.GetExampleTexCoord then
+		c1, c2, c3, c4 = self:GetExampleTexCoord(frame, texture)
+	end
+	if not c4 then
+		return 0, 1, 0, 1
+	end
+	
+	return c1, c2, c3, c4
+end
 --- Clear the icon for the current module if it exists.
 -- @param frame the Unit Frame to clear
 -- @usage local update_layout = MyModule:ClearFrame(frame)
@@ -41,7 +88,7 @@ function IconModule:UpdateFrame(frame)
 	expect(frame, 'typeof', 'frame')
 	--@end-alpha@
 	
-	local tex = self:CallTextureFunction(frame)
+	local tex = call_texture_function(self, frame)
 	if not tex then
 		return self:ClearFrame(frame)
 	end
@@ -60,54 +107,7 @@ function IconModule:UpdateFrame(frame)
 	
 	control:SetTexture(tex)
 	
-	control:SetTexCoord(self:CallTexCoordFunction(frame, tex))
+	control:SetTexCoord(call_tex_coord_function(self, frame, tex))
 	
 	return made_control
-end
-
---- Call the :GetTexture function on the icon module regarding the given frame.
--- @param frame the frame to get the texture of
--- @usage local tex, c1, c2, c3, c4 = MyModule:CallTextureFunction(someFrame)
--- @return texture the path to the texture to show
-function IconModule:CallTextureFunction(frame)
-	if not self.GetTexture then
-		-- no function, let's just return
-		return nil
-	end
-	local tex
-	if frame.guid then
-		tex = self:GetTexture(frame)
-	end
-	if not tex and PitBull4.config_mode and self.GetExampleTexture then
-		tex = self:GetExampleTexture(frame)
-	end
-	if not tex then
-		return nil
-	end
-	
-	return tex
-end
-
---- Call the :GetTexCoord function on the icon module regarding the given frame.
--- @param frame the frame to get the TexCoord of
--- @param texture the texture as returned by :CallTextureFunction
--- @usage local c1, c2, c3, c4 = MyModule:CallTexCoordFunction(someFrame, "SomeTexture")
--- @return left TexCoord for left within [0, 1]
--- @return right TexCoord for right within [0, 1]
--- @return top TexCoord for top within [0, 1]
--- @return bottom TexCoord for bottom within [0, 1]
-function IconModule:CallTexCoordFunction(frame, texture)
-	if not self.GetTexCoord then
-		-- no function, let's just return the defaults
-		return 0, 1, 0, 1
-	end
-	local c1, c2, c3, c4 = self:GetTexCoord(frame, texture)
-	if not c4 and PitBull4.config_mode and self.GetExampleTexCoord then
-		c1, c2, c3, c4 = self:GetExampleTexCoord(frame, texture)
-	end
-	if not c4 then
-		return 0, 1, 0, 1
-	end
-	
-	return c1, c2, c3, c4
 end
