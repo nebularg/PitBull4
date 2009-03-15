28f6674f2d48f1eb986f179c75cc5fc5a5ba8885
Shefki <shefki@shefki.org>
2009-03-14 22:20:06 -0700

    Forget to test if we weren't filter raid groups at all.

--- GroupHeader.lua
+++ GroupHeader.lua
@@ -522,16 +522,18 @@ end
 function GroupHeader:GetMaxUnits()
 	if self.super_unit_group == "raid" then
 		local group_filter = self.group_db.group_filter
-		if group_filter == "" then
+		if group_filter then
+			if group_filter == "" then
 			-- Everything filtered, but always have at least one unit
 			return 1 
-		end
+			end
 
-		-- If we're filtering by raid group we may not need all 40
-		-- units for this group header.
-		local by_raid_group,count = get_filter_type_count(strsplit(",",group_filter))
-		if by_raid_group then
-			return MEMBERS_PER_RAID_GROUP * count
+			-- If we're filtering by raid group we may not need all 40
+			-- units for this group header.
+			local by_raid_group,count = get_filter_type_count(strsplit(",",group_filter))
+			if by_raid_group then
+				return MEMBERS_PER_RAID_GROUP * count
+			end
 		end
 
 		-- Everything else we're gonna have to go by max.
