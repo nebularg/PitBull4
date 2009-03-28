367d600d1dd8ec89575db699c34440bb8d1e984e
Cameron Knight <ckknight@ckknight-laptop.local>
2009-03-27 21:41:38 -0500

    Rename Banzai to Aggro

--- Modules/Aggro/Aggro.lua
+++ Modules/Aggro/Aggro.lua
@@ -2,49 +2,49 @@ if select(6, GetAddOnInfo("PitBull4_" .. (debugstack():match("[o%.][d%.][u%.]les
 
 local PitBull4 = _G.PitBull4
 if not PitBull4 then
-  error("PitBull4_Banzai requires PitBull4")
+  error("PitBull4_Aggro requires PitBull4")
 end
 local L = PitBull4.L
 
 local LibBanzai
 
-local PitBull4_Banzai = PitBull4:NewModule("Banzai", "AceEvent-3.0", "AceHook-3.0")
-PitBull4_Banzai:SetModuleType("custom")
-PitBull4_Banzai:SetName(L["Banzai"])
-PitBull4_Banzai:SetDescription(L["Adds aggro coloring to PitBull4"])
-PitBull4_Banzai:SetDefaults({},{aggro_color = {1, 0, 0, 1}})
-PitBull4_Banzai:SetLayoutOptionsFunction(function(self) end)
+local PitBull4_Aggro = PitBull4:NewModule("Aggro", "AceEvent-3.0", "AceHook-3.0")
+PitBull4_Aggro:SetModuleType("custom")
+PitBull4_Aggro:SetName(L["Aggro"])
+PitBull4_Aggro:SetDescription(L["Adds aggro coloring to PitBull4"])
+PitBull4_Aggro:SetDefaults({},{aggro_color = {1, 0, 0, 1}})
+PitBull4_Aggro:SetLayoutOptionsFunction(function(self) end)
 
 local PitBull4_HealthBar
 
 local function callback(aggro, name, unit)
 	for frame in PitBull4:IterateFramesForGUID(UnitGUID(unit)) do
-		if frame and PitBull4_Banzai:GetLayoutDB(frame).enabled then
+		if frame and PitBull4_Aggro:GetLayoutDB(frame).enabled then
 			PitBull4_HealthBar:UpdateFrame(frame)
 		end
 	end
 end
 
-function PitBull4_Banzai:OnEnable()
+function PitBull4_Aggro:OnEnable()
 	PitBull4_HealthBar = PitBull4:GetModule("HealthBar", true)
 	if not PitBull4_HealthBar then
-		error(L["PitBull4_Banzai requires the HealthBar module"])
+		error(L["PitBull4_Aggro requires the HealthBar module"])
 	end
 	
 	LibBanzai = LibStub("LibBanzai-2.0", true)
 	if not LibBanzai then
-		error(L["PitBull4_Banzai requires the library LibBanzai-2.0 to be available."])
+		error(L["PitBull4_Aggro requires the library LibBanzai-2.0 to be available."])
 	end
 
 	LibBanzai:RegisterCallback(callback)
 	self:RawHook(PitBull4_HealthBar, "GetColor")
 end
 
-function PitBull4_Banzai:OnDisable()
+function PitBull4_Aggro:OnDisable()
 	LibBanzai:UnregisterCallback(callback)
 end
 
-function PitBull4_Banzai:GetColor(module, frame, value)
+function PitBull4_Aggro:GetColor(module, frame, value)
 	local unit = frame.unit
 	if unit and self:GetLayoutDB(frame).enabled and UnitIsFriend("player", unit) and LibBanzai:GetUnitAggroByUnitId(unit) then
 		return unpack(self.db.profile.global.aggro_color)
@@ -53,7 +53,7 @@ function PitBull4_Banzai:GetColor(module, frame, value)
 	return self.hooks[module].GetColor(module, frame, value)
 end
 
-PitBull4_Banzai:SetColorOptionsFunction(function(self)
+PitBull4_Aggro:SetColorOptionsFunction(function(self)
 	return 'aggro_color', {
 		type = 'color',
 		name = L['Aggro'],
