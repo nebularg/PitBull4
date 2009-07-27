c1ae677d5c49fe345a6520e329e101c096460e2c
Shefki <shefki@shefki.org>
2009-07-26 19:42:27 -0700

    Make the Aggro module deal with the states of the other modules it uses to display the aggro better.  Also fixes ticket 329.

--- Modules/Aggro/Aggro.lua
+++ Modules/Aggro/Aggro.lua
@@ -13,6 +13,7 @@ PitBull4_Aggro:SetModuleType("custom")
 PitBull4_Aggro:SetName(L["Aggro"])
 PitBull4_Aggro:SetDescription(L["Add aggro coloring to the unit frame."])
 PitBull4_Aggro:SetDefaults({
+	enabled = nil,
 	kind = "HealthBar",
 },{aggro_color = {1, 0, 0, 1}})
 
@@ -23,19 +24,17 @@ local PitBull4_Background
 local function callback(aggro, name, unit)
 	for frame in PitBull4:IterateFramesForGUID(UnitGUID(unit)) do
 		local db = PitBull4_Aggro:GetLayoutDB(frame)
-		if db.enabled then
-			if db.kind == "HealthBar" then
-				if PitBull4_HealthBar then
-					PitBull4_HealthBar:UpdateFrame(frame)
-				end
-			elseif db.kind == "Border" then
-				if PitBull4_Border then
-					PitBull4_Border:UpdateFrame(frame)
-				end
-			elseif db.kind == "Background" then
-				if PitBull4_Background then
-					PitBull4_Background:UpdateFrame(frame)
-				end
+		if db.kind == "HealthBar" then
+			if PitBull4_HealthBar and PitBull4_HealthBar:IsEnabled() then
+				PitBull4_HealthBar:UpdateFrame(frame)
+			end
+		elseif db.kind == "Border" then
+			if PitBull4_Border and PitBull4_Border:IsEnabled() then
+				PitBull4_Border:UpdateFrame(frame)
+			end
+		elseif db.kind == "Background" then
+			if PitBull4_Background and PitBull4_Background:IsEnabled() then
+				PitBull4_Background:UpdateFrame(frame)
 			end
 		end
 	end
@@ -53,19 +52,16 @@ function PitBull4_Aggro:OnEnable()
 	if PitBull4_HealthBar then
 		self:RawHook(PitBull4_HealthBar, "GetColor", "HealthBar_GetColor")
 	end
-	-- TODO: set up system where the lack of HealthBar is handled sanely
 	
 	PitBull4_Border = PitBull4:GetModule("Border", true)
 	if PitBull4_Border then
 		self:RawHook(PitBull4_Border, "GetTextureAndColor", "Border_GetTextureAndColor")
 	end
-	-- TODO: set up system where the lack of Border is handled sanely
 	
 	PitBull4_Background = PitBull4:GetModule("Background", true)
 	if PitBull4_Background then
 		self:RawHook(PitBull4_Background, "GetColor", "Background_GetColor")
 	end
-	-- TODO: set up system where the lack of Border is handled sanely
 end
 
 function PitBull4_Aggro:OnDisable()
@@ -75,7 +71,7 @@ end
 function PitBull4_Aggro:HealthBar_GetColor(module, frame, value)
 	local unit = frame.unit
 	local db = self:GetLayoutDB(frame)
-	if unit and db.enabled and db.kind == "HealthBar" and UnitIsFriend("player", unit) and LibBanzai:GetUnitAggroByUnitId(unit) then
+	if unit and db.kind == "HealthBar" and UnitIsFriend("player", unit) and LibBanzai:GetUnitAggroByUnitId(unit) then
 		local aggro_color = self.db.profile.global.aggro_color
 		return aggro_color[1], aggro_color[2], aggro_color[3], true
 	end
@@ -86,12 +82,18 @@ end
 function PitBull4_Aggro:Border_GetTextureAndColor(module, frame)
 	local unit = frame.unit
 	local db = self:GetLayoutDB(frame)
+
+	local texture, r, g, b, a
+	if module:GetLayoutDB(frame).enabled then
+		-- Only call GetTextureAndColor if the Border is enabled for the layout already.
+		-- This allows the border to enable for the aggro display and then disable back
+		-- to the normal settings
+		texture, r, g, b, a = self.hooks[module].GetTextureAndColor(module, frame)
+	end
 	
-	local texture, r, g, b, a = self.hooks[module].GetTextureAndColor(module, frame)
-	
-	if unit and db.enabled and db.kind == "Border" and UnitIsFriend("player", unit) and LibBanzai:GetUnitAggroByUnitId(unit) then
+	if unit and db.kind == "Border" and UnitIsFriend("player", unit) and LibBanzai:GetUnitAggroByUnitId(unit) then
 		r, g, b, a = unpack(self.db.profile.global.aggro_color)
-		if texture == "None" then
+		if not texture or texture == "None" then
 			texture = "Blizzard Tooltip"
 		end
 	end
@@ -102,25 +104,53 @@ end
 function PitBull4_Aggro:Background_GetColor(module, frame)
 	local unit = frame.unit
 	local db = self:GetLayoutDB(frame)
+
+	local r, g, b, a
+	if module:GetLayoutDB(frame).enabled then
+		-- Only call GetColor if the Background is enabled for the layout already.
+		-- This allows the background to enable for the aggro display and then disable back
+		-- to the normal settings
+		r, g, b, a = self.hooks[module].GetColor(module, frame)
+	end
 	
-	local r, g, b, a = self.hooks[module].GetColor(module, frame)
-	
-	if unit and db.enabled and db.kind == "Background" and UnitIsFriend("player", unit) and LibBanzai:GetUnitAggroByUnitId(unit) then
+	if unit and db.kind == "Background" and UnitIsFriend("player", unit) and LibBanzai:GetUnitAggroByUnitId(unit) then
 		local a2
 		r, g, b, a2 = unpack(self.db.profile.global.aggro_color)
-		a = a * a2
+		if a then
+			a = a * a2
+		else
+			a = a2
+		end
 	end
 	
 	return r, g, b, a
 end
 
 PitBull4_Aggro:SetLayoutOptionsFunction(function(self)
-	return 'kind', {
+	local function is_kind_allowed(kind)
+		if kind == "HealthBar" then
+			return PitBull4_HealthBar and PitBull4_HealthBar:IsEnabled() and PitBull4.Options.GetLayoutDB(PitBull4_HealthBar.id).enabled
+		elseif kind == "Border" then
+			return PitBull4_Border and PitBull4_Border:IsEnabled()
+		elseif kind == "Background" then
+			return PitBull4_Background and PitBull4_Background:IsEnabled()
+		elseif kind == "" then
+			return true
+		else
+			return false
+		end
+	end
+
+	return 'enable', nil, 'kind', {
 		type = 'select',
 		name = L["Display"],
 		desc = L["How to display the aggro indication."],
 		get = function(info)
-			return PitBull4.Options.GetLayoutDB(self).kind
+			local kind = PitBull4.Options.GetLayoutDB(self).kind
+			if not is_kind_allowed(kind) then
+				return ""
+			end
+			return kind
 		end,
 		set = function(info, value)
 			PitBull4.Options.GetLayoutDB(self).kind = value
@@ -129,13 +159,14 @@ PitBull4_Aggro:SetLayoutOptionsFunction(function(self)
 		end,
 		values = function(info)
 			local t = {}
-			if PitBull4_HealthBar then
+			t[""] = L["None"]
+			if is_kind_allowed("HealthBar") then 
 				t.HealthBar = L["Health bar"]
 			end
-			if PitBull4_Border then
+			if is_kind_allowed("Border") then 
 				t.Border = L["Border"]
 			end
-			if PitBull4_Background then
+			if is_kind_allowed("Background") then 
 				t.Background = L["Background"]
 			end
 			return t
