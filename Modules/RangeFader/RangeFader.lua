3b9faef3bea513baa37122dc3ce33ea00efe6250
Shefki <shefki@shefki.org>
2009-09-11 22:55:30 -0700

    Ticket 583: Improved options for the RangeFader.

--- Modules/RangeFader/RangeFader.lua
+++ Modules/RangeFader/RangeFader.lua
@@ -15,11 +15,19 @@ PitBull4_RangeFader:SetDescription(L["Make the unit frame fade if out of range."
 PitBull4_RangeFader:SetDefaults({
 	enabled = false,
 	out_of_range_opacity = 0.6,
+	check_method = 'class',
 })
 
 function PitBull4_RangeFader:OnEnable()
 	self:ScheduleRepeatingTimer("UpdateNonWacky", 0.7)
 end
+
+local check_method_to_dist_index = {
+	inspect = 1,
+	trade = 2,
+	duel = 3,
+	follow = 4,
+}
 
 local friendly_is_in_range, pet_is_in_range, enemy_is_in_range
 local enemy_is_in_long_range
@@ -155,25 +163,54 @@ end
 
 function PitBull4_RangeFader:GetOpacity(frame)
 	local unit = frame.unit
-	if UnitIsEnemy("player", unit) then
-		if enemy_is_in_range(unit) then
+	local db = self:GetLayoutDB(frame)
+	local check_method = db.check_method
+
+	if check_method== "follow" or check_method == "trade" or check_method == "duel" or check_method == "follow" then
+		if CheckInteractDistance(unit, check_method_to_dist_index[check_method]) then
 			return 1
-		elseif enemy_is_in_long_range(unit) then
-			return (self:GetLayoutDB(frame).out_of_range_opacity + frame.layout_db.opacity_max) / 2
 		else
-			return self:GetLayoutDB(frame).out_of_range_opacity
+			return db.out_of_range_opacity
 		end
-	elseif UnitIsUnit(unit, "pet") then
-		if pet_is_in_range(unit) then
+	elseif check_method == "custom_spell" and db.custom_spell then
+		if IsSpellInRange(db.custom_spell, unit) == 1 then
 			return 1
 		else
-			return self:GetLayoutDB(frame).out_of_range_opacity
+			return db.out_of_range_opacity
 		end
-	else
-		if friendly_is_in_range(unit) then
+	elseif check_method == "custom_item" and db.custom_item then
+		if IsItemInRange(db.custom_item, unit) == 1 then
 			return 1
 		else
-			return self:GetLayoutDB(frame).out_of_range_opacity
+			return db.out_of_range_opacity
+		end
+	elseif check_method == "helpful" then
+		if UnitInRange(unit) then
+			return 1
+		else
+			return db.out_of_range_opacity
+		end
+	else -- class
+		if UnitIsEnemy("player", unit) then
+			if enemy_is_in_range(unit) then
+				return 1
+			elseif enemy_is_in_long_range(unit) then
+				return (db.out_of_range_opacity + frame.layout_db.opacity_max) / 2
+			else
+				return db.out_of_range_opacity
+			end
+		elseif UnitIsUnit(unit, "pet") then
+			if pet_is_in_range(unit) then
+				return 1
+			else
+				return db.out_of_range_opacity
+			end
+		else
+			if friendly_is_in_range(unit) then
+				return 1
+			else
+				return db.out_of_range_opacity
+			end
 		end
 	end
 end
@@ -201,5 +238,73 @@ PitBull4_RangeFader:SetLayoutOptionsFunction(function(self)
 		end,
 		step = 0.01,
 		bigStep = 0.05,
+	}, 'check_method', {
+		type = 'select',
+		name = L["Range check method"],
+		desc = L['Choose the method to determine if the unit is in range.'],
+		values = {
+			helpful = L['Helpful spells (~40 yards)'],
+			class = L['Class abilities'],
+			follow = L['Follow (~28 yards)'],
+			trade = L['Trade (~11 yards)'],
+			duel = L['Duel (~10 yards)'],
+			custom_spell = L['Custom spell'],
+			custom_item = L['Custom item'],
+		},
+		get = function(info)
+			local db = PitBull4.Options.GetLayoutDB(self)
+
+			return db.check_method
+		end,
+		set = function(info, value)
+			local db = PitBull4.Options.GetLayoutDB(self)
+
+			db.check_method = value
+			
+			PitBull4.Options.UpdateFrames()
+			PitBull4:RecheckAllOpacities()
+		end,
+	}, 'custom_spell', {
+		type = 'input',
+		name = L['Custom spell'],
+		desc = L['Enter the name of the spell you want use to check the range with.'],
+		get = function(info)
+			local db = PitBull4.Options.GetLayoutDB(self)
+
+			return db.custom_spell
+		end,
+		set = function(info, value)
+			local db = PitBull4.Options.GetLayoutDB(self)
+
+			db.custom_spell = value
+
+			PitBull4.Options.UpdateFrames()
+			PitBull4:RecheckAllOpacities()
+		end,
+		hidden = function(info)
+			local db = PitBull4.Options.GetLayoutDB(self)
+			return db.check_method ~= "custom_spell"
+		end,
+	}, 'custom_item', {
+		type = 'input',
+		name = L['Custom item'],
+		desc = L['Enter the name of the item you want use to check the range with.'],
+		get = function(info)
+			local db = PitBull4.Options.GetLayoutDB(self)
+
+			return db.custom_item
+		end,
+		set = function(info, value)
+			local db = PitBull4.Options.GetLayoutDB(self)
+
+			db.custom_item = value
+
+			PitBull4.Options.UpdateFrames()
+			PitBull4:RecheckAllOpacities()
+		end,
+		hidden = function(info)
+			local db = PitBull4.Options.GetLayoutDB(self)
+			return db.check_method ~= "custom_item"
+		end,
 	}
 end)
