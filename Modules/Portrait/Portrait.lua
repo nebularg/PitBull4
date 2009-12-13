2eca2ed7d881ba53f6b79d7696bdf36a5cd9018b
Shefki <shefki@shefki.org>
2009-12-12 18:24:25 -0800

    Ticket 644: Full Body Portraits not always proper zoom.

--- Modules/Portrait/Portrait.lua
+++ Modules/Portrait/Portrait.lua
@@ -123,17 +123,17 @@ local function model_OnUpdate(self, elapsed)
 		self:TryOn(6836)
 		self:TryOn(2955)
 		self:TryOn(3935)
-		if full_body then
-			self:SetCamera(1)
-		else
+		if not full_body then
 			self:SetCamera(0)
 		end
 	elseif style == "three_dimensional" then
 		if not frame.Portrait.falling_back then
 			self:SetUnit(frame.unit)
-			if full_body then
-				self:SetCamera(1)
-			else
+			if not full_body then
+				-- For portrait mode set the models camera to the 0 preset.
+				-- Note: Full body is technically preset 1, but under no circumstances
+				-- should we directly set it.  The camera will already be set to preset
+				-- 1 and doing it ourselves causes some models to show up as just a blob.
 				self:SetCamera(0)
 			end
 		else
