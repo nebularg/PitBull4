d38e62205406b14a3153156adede256e462e53a3
Shefki <shefki@shefki.org>
2010-03-19 05:12:20 -0700

    Revert "Make the Aggro module function when run in an unpackaged format."
    This reverts commit 5a648e4b0fa5b9a8270a6fa107d0dc3186a99bed.
    
    This was drycoded based on a report from ckknight.  It's not necessary
    and was causing errors.  Fixes ticket 708.

--- ModuleHandling/Embedded.lua
+++
@@ -1,4 +0,0 @@
--- Notify modules that all embedded modules have been loaded since there are no
--- built in events for those modules loaded because WoW doesn't see them as addons
--- in their own right.
-PitBull4:CallMethodOnModules("OnEmbeddedModulesLoaded")
