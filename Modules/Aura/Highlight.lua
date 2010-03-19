de02dee048e94c1125d1af6b401a935f4f9b7c65
Shefki <shefki@shefki.org>
2010-03-19 04:20:20 -0700

    Allow the Aura filters used for Highlights access to the consolidate and spell id filter types for frames that aren't showing auras but do have a highlight configured.

--- Modules/Aura/Highlight.lua
+++ Modules/Aura/Highlight.lua
@@ -59,7 +59,7 @@ function PitBull4_Aura:HighlightFilterIterator(frame, db, is_buff)
 		-- Note entry[2] says if the aura is a weapon enchant
 		entry[1], entry[2], entry[3], entry[4], entry[5], entry[6],
 			entry[7], entry[8], entry[9], entry[10], entry[11],
-			entry[12], entry[13] = 
+			entry[12], entry[13], entry[14], entry[15]  = 
 			id, nil, nil, is_buff, UnitAura(unit, id, filter)
 
 		-- Hack to get around a Blizzard bug.  The Enrage debuff_type
