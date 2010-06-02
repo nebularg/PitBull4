19ebec97a79465e1b4dbeeba8013cf5a52419940
Shefki <shefki@shefki.org>
2010-06-01 23:10:37 -0700

    Add the ability to disable individual raid target icons per layout.

--- Modules/RaidTargetIcon/RaidTargetIcon.lua
+++ Modules/RaidTargetIcon/RaidTargetIcon.lua
@@ -16,6 +16,14 @@ PitBull4_RaidTargetIcon:SetDefaults({
 	attach_to = "root",
 	location = "edge_top",
 	position = 1,
+	[1] = true, -- Star 
+	[2] = true, -- Circle 
+	[3] = true, -- Diamond 
+	[4] = true, -- Triangle 
+	[5] = true, -- Moon
+	[6] = true, -- Square
+	[7] = true, -- Cross
+	[8] = true, -- Skull
 })
 
 function PitBull4_RaidTargetIcon:OnEnable()
@@ -29,6 +37,11 @@ function PitBull4_RaidTargetIcon:GetTexture(frame)
 	local index = GetRaidTargetIndex(unit)
 	
 	if not index then
+		return nil
+	end
+
+	-- Disabled
+	if not self:GetLayoutDB(frame)[index] then
 		return nil
 	end
 	
@@ -58,3 +71,65 @@ end
 function PitBull4_RaidTargetIcon:PARTY_MEMBERS_CHANGED()
 	self:ScheduleTimer("UpdateAll", 0.1)
 end
+
+PitBull4_RaidTargetIcon:SetLayoutOptionsFunction(function(self)
+	local function get(info)
+		return PitBull4.Options.GetLayoutDB(self)[info[#info]+0]
+	end
+
+	local function set(info,value)
+		PitBull4.Options.GetLayoutDB(self)[info[#info]+0] = value
+
+		PitBull4.Options.UpdateFrames()
+	end
+
+	return '1',{
+		type = 'toggle',
+		name = [[|TInterface\TargetingFrame\UI-RaidTargetingIcons:0:0:0:0:256:256:0:64:0:64|t |cfffff200]]..RAID_TARGET_1,
+		desc = L["Show this raid target icon for this layout."],
+		get = get,
+		set = set,
+	}, '2', {
+		type = 'toggle',
+		name = [[|TInterface\TargetingFrame\UI-RaidTargetingIcons:0:0:0:0:256:256:64:128:0:64|t |cfff99100]]..RAID_TARGET_2,
+		desc = L["Show this raid target icon for this layout."],
+		get = get,
+		set = set,
+	}, '3', {
+		type = 'toggle',
+		name = [[|TInterface\TargetingFrame\UI-RaidTargetingIcons:0:0:0:0:256:256:128:192:0:64|t |cffd338e5]]..RAID_TARGET_3,
+		desc = L["Show this raid target icon for this layout."],
+		get = get,
+		set = set,
+	}, '4', {
+		type = 'toggle',
+		name = [[|TInterface\TargetingFrame\UI-RaidTargetingIcons:0:0:0:0:256:256:192:256:0:64|t |cff0af200]]..RAID_TARGET_4,
+		desc = L["Show this raid target icon for this layout."],
+		get = get,
+		set = set,
+	}, '5', {
+		type = 'toggle',
+		name = [[|TInterface\TargetingFrame\UI-RaidTargetingIcons:0:0:0:0:256:256:0:64:64:128|t |cffb2d1df]]..RAID_TARGET_5,
+		desc = L["Show this raid target icon for this layout."],
+		get = get,
+		set = set,
+	}, '6', {
+		type = 'toggle',
+		name = [[|TInterface\TargetingFrame\UI-RaidTargetingIcons:0:0:0:0:256:256:64:128:64:128|t |cff00b5ff]]..RAID_TARGET_6,
+		desc = L["Show this raid target icon for this layout."],
+		get = get,
+		set = set,
+	}, '7', {
+		type = 'toggle',
+		name = [[|TInterface\TargetingFrame\UI-RaidTargetingIcons:0:0:0:0:256:256:128:192:64:128|t |cffff3d2a]]..RAID_TARGET_7,
+		desc = L["Show this raid target icon for this layout."],
+		get = get,
+		set = set,
+	}, '8', {
+		type = 'toggle',
+		name = [[|TInterface\TargetingFrame\UI-RaidTargetingIcons:0:0:0:0:256:256:192:256:64:128|t |cfff9f9f9]]..RAID_TARGET_8,
+		desc = L["Show this raid target icon for this layout."],
+		get = get,
+		set = set,
+	}
+end)
