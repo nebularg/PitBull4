485dd579f2d3c4b339d4d098ec0b6a5a17484c07
Shefki <shefki@shefki.org>
2010-06-15 21:28:04 -0700

    Fix an error in the Aura FilterEditor when changing profiles.

--- Modules/Aura/FilterEditor.lua
+++ Modules/Aura/FilterEditor.lua
@@ -171,6 +171,10 @@ PitBull4_Aura.OnProfileChanged_funcs[#PitBull4_Aura.OnProfileChanged_funcs+1] =
 function(self)
 	-- Recalculate the filter options on a profile change
 	if CURRENT_FILTER then
+		if not PitBull4_Aura:GetFilterDB(filter) then
+			-- No filter of the same name in new profile so fall back to the default.
+			CURRENT_FILTER = '!B'
+		end
 		self:SetFilterOptions(CURRENT_FILTER,CURRENT_FILTER_OPTIONS)
 	end
 end
