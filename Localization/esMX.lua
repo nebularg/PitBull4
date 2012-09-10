837c6d21b7012e7c463beb6efe7a20fb0e0087d1
Shefki <shefki@shefki.org>
2012-09-10 01:26:21 -0700

    Allow localization for Latin American Spanish, Italian, and Brazilian Portuguese.

---
+++ Localization/esMX.lua
@@ -0,0 +1,6 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("PitBull4", "esMX")
+if not L then return end
+
+--[===[@non-debug@
+@localization(locale="esMX", format="lua_additive_table")@
+--@end-non-debug@]===]
---
+++ Localization/itIT.lua
@@ -0,0 +1,6 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("PitBull4", "itIT")
+if not L then return end
+
+--[===[@non-debug@
+@localization(locale="itIT", format="lua_additive_table")@
+--@end-non-debug@]===]
