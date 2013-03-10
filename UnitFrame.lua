2bb5aa72b24d3cc0593f3e4a0a198e5efde5f5e1
Shefki <shefki@shefki.org>
2013-03-09 17:24:07 -0800

    Remove pre-mop compatibility from UnitFrame.lua
    Conflicts:
    
    	UnitFrame.lua

--- UnitFrame.lua
+++ UnitFrame.lua
@@ -4,36 +4,7 @@ local L = PitBull4.L
 
 local DEBUG = PitBull4.DEBUG
 local expect = PitBull4.expect
-local cata_400 = select(4,GetBuildInfo()) >= 40000
 
-local mop_500 = select(4,GetBuildInfo()) >= 50000
-local IsInGroup = IsInGroup
-local IsInRaid = IsInRaid
-local UnitIsGroupLeader = UnitIsGroupLeader
-local UnitIsGroupAssistant = UnitIsGroupAssistant
-if not mop_500 then
-	IsInGroup = function()
-		return GetNumPartyMembers() > 0
-	end
-	IsInRaid = function()
-		return GetNumRaidMembers() > 0
-	end
-	UnitIsGroupLeader = function(unit)
-		-- shortcutted probably should support full range of functionality
-		-- but I don't need it for now.
-		if unit == "player" then
-			return IsPartyLeader()
-		end
-	end
-	UnitIsGroupAssistant = function(unit)
-		-- shortcutted probably should support full range of functionality
-		-- but I don't need it for now.
-		if unit == "player" then
-			return IsRaidOfficer()
-		end
-	end
-
-end
 local mop_520 = select(4,GetBuildInfo()) >= 50200
 
 -- CONSTANTS ----------------------------------------------------------------
@@ -590,7 +561,7 @@ function PitBull4:ConvertIntoUnitFrame(frame, isExampleFrame)
 			frame:RegisterForClicks("AnyUp")
 			frame:SetAttribute("*type1", "target")
 			frame:SetAttribute("*type2", mop_520 and "togglemenu" or "menu")
-		elseif cata_400 and not ClickCastHeader then
+		elseif not ClickCastHeader then
 			-- if we can't set attributes directly and Clique isn't running
 			-- then RegisterForClicks upon leaving combat.  Works around
 			-- the lack of RegisterForClicks in the RestrictedFrames environment
@@ -701,7 +672,7 @@ SingletonUnitFrame.RefixSizeAndPosition = PitBull4:OutOfCombatWrapper(SingletonU
 -- @usage frame:Activate()
 function SingletonUnitFrame:Activate()
 	RegisterUnitWatch(self, true)
-	RegisterStateDriver(self, "pb4visibility", mop_500 and "[petbattle] hide; default" or "default")
+	RegisterStateDriver(self, "pb4visibility", "[petbattle] hide; default")
 end
 SingletonUnitFrame.Activate = PitBull4:OutOfCombatWrapper(SingletonUnitFrame.Activate)
 
