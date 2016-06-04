977a71021264942128b96d1ce827d86fd1501441
Kyle Buller <bullerk@gmail.com>
2016-06-04 16:20:28 -0500

    Rename DruidManaBar to AltManaBar

--- Modules/AltManaBar/AltManaBar.lua
+++ Modules/AltManaBar/AltManaBar.lua
@@ -7,17 +7,17 @@ end
 
 local PitBull4 = _G.PitBull4
 if not PitBull4 then
-	error("PitBull4_DruidManaBar requires PitBull4")
+	error("PitBull4_AltManaBar requires PitBull4")
 end
 
 local L = PitBull4.L
 
-local PitBull4_DruidManaBar = PitBull4:NewModule("DruidManaBar", "AceEvent-3.0")
+local PitBull4_AltManaBar = PitBull4:NewModule("DruidManaBar", "AceEvent-3.0")
 
-PitBull4_DruidManaBar:SetModuleType("bar")
-PitBull4_DruidManaBar:SetName(L["Druid/Monk mana bar"])
-PitBull4_DruidManaBar:SetDescription(L["Show the mana bar when a druid is in cat or bear form or a mistweaver monk is in stance of the fierce tiger."])
-PitBull4_DruidManaBar:SetDefaults({
+PitBull4_AltManaBar:SetModuleType("bar")
+PitBull4_AltManaBar:SetName(L["Alternate mana bar"])
+PitBull4_AltManaBar:SetDescription(L["Show the mana bar for specs that don't use mana as their primary resource."])
+PitBull4_AltManaBar:SetDefaults({
 	size = 1,
 	position = 6,
 	hide_if_full = false,
@@ -29,20 +29,20 @@ local MANA_TYPE = 0
 -- cached power type for optimization
 local power_type = nil
 
-function PitBull4_DruidManaBar:OnEnable()
-	PitBull4_DruidManaBar:RegisterEvent("UNIT_POWER_FREQUENT")
-	PitBull4_DruidManaBar:RegisterEvent("UNIT_MAXPOWER","UNIT_POWER_FREQUENT")
-	PitBull4_DruidManaBar:RegisterEvent("UNIT_DISPLAYPOWER","UNIT_POWER_FREQUENT")
+function PitBull4_AltManaBar:OnEnable()
+	self:RegisterEvent("UNIT_POWER_FREQUENT")
+	self:RegisterEvent("UNIT_MAXPOWER","UNIT_POWER_FREQUENT")
+	self:RegisterEvent("UNIT_DISPLAYPOWER","UNIT_POWER_FREQUENT")
 	if player_class == "MONK" then
-		PitBull4_DruidManaBar:RegisterEvent("PLAYER_SPECIALIZATION_CHANGED")
+		self:RegisterEvent("PLAYER_SPECIALIZATION_CHANGED")
 	end
 end
 
-function PitBull4_DruidManaBar:GetValue(frame)
+function PitBull4_AltManaBar:GetValue(frame)
 	if frame.unit ~= "player" then
 		return nil
 	end
- 
+
 	power_type = UnitPowerType("player")
 	if power_type == MANA_TYPE then
 		return nil
@@ -64,18 +64,18 @@ function PitBull4_DruidManaBar:GetValue(frame)
 
 	return percent
 end
-function PitBull4_DruidManaBar:GetExampleValue(frame)
+function PitBull4_AltManaBar:GetExampleValue(frame)
 	-- just go with what :GetValue gave
 	return nil
 end
 
-function PitBull4_DruidManaBar:GetColor(frame, value)
+function PitBull4_AltManaBar:GetColor(frame, value)
 	local color = PitBull4.PowerColors["MANA"]
 	return color[1], color[2], color[3]
 end
-PitBull4_DruidManaBar.GetExampleColor = PitBull4_DruidManaBar.GetColor
+PitBull4_AltManaBar.GetExampleColor = PitBull4_AltManaBar.GetColor
 
-function PitBull4_DruidManaBar:UNIT_POWER_FREQUENT(event, unit, power_type)
+function PitBull4_AltManaBar:UNIT_POWER_FREQUENT(event, unit, power_type)
 	if unit ~= "player" or ((event == "UNIT_POWER_FREQUENT" or event == "UNIT_MAXPOWER") and power_type ~= "MANA") then
 		return
 	end
@@ -84,8 +84,7 @@ function PitBull4_DruidManaBar:UNIT_POWER_FREQUENT(event, unit, power_type)
 	power_type = UnitPowerType("player") 
 	if power_type == MANA_TYPE and power_type == prev_power_type then
 		-- We really don't want to iterate all the frames on every mana
-		-- update when the druid is already in a mana form and the bar
-		-- is already hidden.
+		-- update when the bar is already hidden.
 		return
 	end
 
@@ -94,13 +93,13 @@ function PitBull4_DruidManaBar:UNIT_POWER_FREQUENT(event, unit, power_type)
 	end
 end
 
-function PitBull4_DruidManaBar:PLAYER_SPECIALIZATION_CHANGED(event)
+function PitBull4_AltManaBar:PLAYER_SPECIALIZATION_CHANGED()
 	for frame in PitBull4:IterateFramesForUnitID("player") do
 		self:Update(frame)
 	end
 end
 
-PitBull4_DruidManaBar:SetLayoutOptionsFunction(function(self)
+PitBull4_AltManaBar:SetLayoutOptionsFunction(function(self)
 	return 'hide_if_full', {
 		name = L["Hide if full"],
 		desc = L["Hide when at 100% mana."],
