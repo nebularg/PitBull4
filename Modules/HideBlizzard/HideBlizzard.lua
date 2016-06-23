bfc12f02e2c7a198c20061ae753fcb5c91b296f6
Kyle Buller <bullerk@gmail.com>
2016-06-23 11:08:17 -0500

    Refactored HideBlizzard module

--- Modules/HideBlizzard/HideBlizzard.lua
+++ Modules/HideBlizzard/HideBlizzard.lua
@@ -5,8 +5,10 @@ if not PitBull4 then
 	error("PitBull4_HideBlizzard requires PitBull4")
 end
 
+local legion_700 = select(4, GetBuildInfo()) >= 70000
+
 local L = PitBull4.L
-local PitBull4_HideBlizzard = PitBull4:NewModule("HideBlizzard")
+local PitBull4_HideBlizzard = PitBull4:NewModule("HideBlizzard", "AceHook-3.0")
 
 PitBull4_HideBlizzard:SetModuleType("custom")
 PitBull4_HideBlizzard:SetName(L["Hide Blizzard frames"])
@@ -15,13 +17,13 @@ PitBull4_HideBlizzard:SetDefaults({}, {
 	player = true,
 	party = true,
 	raid = false,
-	target = true, 
+	target = true,
 	focus = true,
 	castbar = true,
 	aura = false,
 	runebar = true,
 	altpower = false,
-	boss = true,
+	boss = false,
 })
 
 function PitBull4_HideBlizzard:OnEnable()
@@ -39,6 +41,9 @@ end
 local showers = {}
 local hiders = {}
 local currently_hidden = {}
+local parents = {}
+
+
 
 function PitBull4_HideBlizzard:UpdateFrames()
 	for name in pairs(showers) do
@@ -57,38 +62,76 @@ function PitBull4_HideBlizzard:UpdateFrames()
 end
 PitBull4_HideBlizzard.UpdateFrames = PitBull4:OutOfCombatWrapper(PitBull4_HideBlizzard.UpdateFrames)
 
-local function hook_playerframe()
-	hooksecurefunc("PlayerFrame_HideVehicleTexture",function()
-		if currently_hidden["runebar"] then
-			hiders["runebar"]()
+local noop = function() end
+local hide_frame = PitBull4:OutOfCombatWrapper(function(self) self:Hide() end)
+
+local hidden_frame = CreateFrame("Frame")
+hidden_frame:Hide()
+
+local function hook_frames(...)
+	for i = 1, select("#", ...) do
+		local frame = select(i, ...)
+		frame:UnregisterAllEvents()
+		if not PitBull4_HideBlizzard:IsHooked(frame, "OnShow") then
+			PitBull4_HideBlizzard:SecureHookScript(frame, "OnShow", hide_frame)
 		end
-	end)
-	hook_playerframe = nil
+		frame:Hide()
+	end
 end
 
-local function hook_raidmanager()
-	hooksecurefunc("CompactRaidFrameManager_UpdateShown",function()
-		if currently_hidden["raid"] then
-			hiders["raid"]()
+local function hook_reparent_frames(...)
+	for i = 1, select("#", ...) do
+		local frame = select(i, ...)
+		frame:UnregisterAllEvents()
+		if not PitBull4_HideBlizzard:IsHooked(frame, "OnShow") then
+			PitBull4_HideBlizzard:SecureHookScript(frame, "OnShow", hide_frame)
+			parents[frame] = frame:GetParent()
+			frame:SetParent(hidden_frame)
 		end
-	end)
-	hook_raidmanager = nil
+		frame:Hide()
+	end
 end
 
-local function hook_compactparty()
-	hooksecurefunc("CompactPartyFrame_UpdateShown",function()
-		if currently_hidden["party"] then
-			hiders["party"]()
+local function rawhook_frames(...)
+	for i = 1, select("#", ...) do
+		local frame = select(i, ...)
+		frame:UnregisterAllEvents()
+		PitBull4_HideBlizzard:RawHook(frame, "Show", noop, true)
+		frame:Hide()
+	end
+end
+
+local function unhook_frame(frame)
+	if PitBull4_HideBlizzard:IsHooked(frame, "OnShow") then
+		PitBull4_HideBlizzard:Unhook(frame, "OnShow")
+		local parent = parents[frame]
+		if parent then
+			frame:SetParent(parent)
 		end
-	end)
-	hook_compactparty = nil
+	elseif PitBull4_HideBlizzard:IsHooked(frame, "Show") then
+		PitBull4_HideBlizzard:Unhook(frame, "Show")
+	end
+end
+
+local function unhook_frames(...)
+	for i = 1, select("#", ...) do
+		local frame = select(i, ...)
+		unhook_frame(frame)
+		local handler = frame:GetScript("OnLoad")
+		if handler then
+			handler(frame)
+		end
+	end
+end
+
+local function unhook_frames_without_init(...)
+	for i = 1, select("#", ...) do
+		local frame = select(i, ...)
+		unhook_frame(frame)
+	end
 end
 
 function hiders:player()
-	-- Only hide the PlayerFrame, do not mess with the events.
-	-- Unfortunately, messing the PlayerFrame ends up spreading
-	-- taint to the BuffFrame which matters now that CancelUnitBuff
-	-- is protected.
 	PlayerFrame:Hide()
 end
 
@@ -97,150 +140,185 @@ function showers:player()
 end
 
 function hiders:party()
-	for i = 1, 4 do
-		local frame = _G["PartyMemberFrame"..i]
-		frame:UnregisterAllEvents()
-		frame:Hide()
-		frame:SetAttribute("statehidden", true)
-		frame:SetScript("OnEvent", nil);
+	for i = 1, MAX_PARTY_MEMBERS do
+		local frame = _G["PartyMemberFrame" .. i]
+		rawhook_frames(frame)
 	end
-	
+
 	UIParent:UnregisterEvent("GROUP_ROSTER_UPDATE")
 end
 
 function showers:party()
-	for i = 1, 4 do
-		local frame = _G["PartyMemberFrame"..i]
-		frame:SetAttribute("statehidden", nil)
-		frame:GetScript("OnLoad")(frame)
+	for i = 1, MAX_PARTY_MEMBERS do
+		local frame = _G["PartyMemberFrame" .. i]
+		unhook_frames(frame)
 		frame:GetScript("OnEvent")(frame, "GROUP_ROSTER_UPDATE")
-		
-		PartyMemberFrame_UpdateMember(frame)
 	end
 
 	UIParent:RegisterEvent("GROUP_ROSTER_UPDATE")
 end
 
-local compact_raid
-function hiders:raid()
-	CompactRaidFrameManager:UnregisterEvent("GROUP_ROSTER_UPDATE")
-	CompactRaidFrameManager:UnregisterEvent("PLAYER_ENTERING_WORLD")
-	CompactRaidFrameManager:Hide()
-	compact_raid = CompactRaidFrameManager_GetSetting("IsShown")
-	if compact_raid and compact_raid ~= "0" then 
-		CompactRaidFrameManager_SetSetting("IsShown", "0")
-	end
-	if hook_raidmanager then
-		hook_raidmanager()
-	end
-end
+do
+	local raid_shown = nil
+	local function hide_raid()
+			CompactRaidFrameManager:UnregisterEvent("GROUP_ROSTER_UPDATE")
+			CompactRaidFrameManager:UnregisterEvent("PLAYER_ENTERING_WORLD")
+			if InCombatLockdown() then return end
 
-function showers:raid()
-	CompactRaidFrameManager:RegisterEvent("GROUP_ROSTER_UPDATE")	
-	CompactRaidFrameManager:RegisterEvent("PLAYER_ENTERING_WORLD")
-	if GetDisplayedAllyFrames() == "raid" then
-		CompactRaidFrameManager:Show()
+			raid_shown = CompactRaidFrameManager_GetSetting("IsShown")
+			if raid_shown and raid_shown ~= "0" then
+				CompactRaidFrameManager_SetSetting("IsShown", "0")
+			end
+			CompactRaidFrameManager:Hide()
 	end
-	if compact_raid and compact_raid ~= "0" then
-		CompactRaidFrameManager_SetSetting("IsShown", "1")
+
+	function hiders:raid()
+		if not PitBull4_HideBlizzard:IsHooked("CompactRaidFrameManager_UpdateShown") then
+			PitBull4_HideBlizzard:SecureHook("CompactRaidFrameManager_UpdateShown", hide_raid)
+			PitBull4_HideBlizzard:SecureHookScript(CompactRaidFrameManager, "OnShow", hide_frame)
+		end
+		hide_raid()
+	end
+
+	function showers:raid()
+		PitBull4_HideBlizzard:Unhook("CompactRaidFrameManager_UpdateShown")
+		PitBull4_HideBlizzard:Unhook(CompactRaidFrameManager, "OnShow")
+
+		CompactRaidFrameManager:RegisterEvent("GROUP_ROSTER_UPDATE")
+		CompactRaidFrameManager:RegisterEvent("PLAYER_ENTERING_WORLD")
+
+		if raid_shown and raid_shown ~= "0" then
+			CompactRaidFrameManager_SetSetting("IsShown", "1")
+		end
+
+		if GetDisplayedAllyFrames() then
+			CompactRaidFrameManager:Show()
+		end
 	end
 end
 
 function hiders:target()
-	TargetFrame:UnregisterAllEvents()
-	TargetFrame:Hide()
-
-	ComboFrame:UnregisterAllEvents()
+	hook_frames(TargetFrame, ComboFrame)
 end
 
 function showers:target()
-	TargetFrame:GetScript("OnLoad")(TargetFrame)
-
-	ComboFrame:GetScript("OnLoad")(ComboFrame)
+	unhook_frames(TargetFrame, ComboFrame)
+	ComboFrame:Show()
 end
 
 function hiders:focus()
-	FocusFrame:UnregisterAllEvents()
-	FocusFrame:Hide()
+	hook_frames(FocusFrame)
 end
 
 function showers:focus()
-	FocusFrame:GetScript("OnLoad")(FocusFrame)
+	unhook_frames(FocusFrame)
 end
 
 function hiders:castbar()
-	CastingBarFrame:UnregisterAllEvents()
-	PetCastingBarFrame:UnregisterAllEvents()
+	rawhook_frames(CastingBarFrame, PetCastingBarFrame)
 end
 
 function showers:castbar()
-	CastingBarFrame:GetScript("OnLoad")(CastingBarFrame)
-	PetCastingBarFrame:GetScript("OnLoad")(PetCastingBarFrame)
+	unhook_frames(CastingBarFrame, PetCastingBarFrame)
 end
 
 function hiders:runebar()
-	if hook_playerframe then
-		hook_playerframe()
+	if not legion_700 then
+		hook_frames(WarlockPowerFrame, TotemFrame, EclipseBarFrame, PaladinPowerBar, RuneFrame, PriestBarFrame, MonkHarmonyBar)
+	else
+		hook_frames(PlayerFrame.classPowerBar, TotemFrame, EclipseBarFrame, RuneFrame, PriestBarFrame)
 	end
-	RuneFrame:UnregisterAllEvents()
-	RuneFrame:Hide()
 end
 
 function showers:runebar()
-	local _,class = UnitClass("player")
-	if class == "DEATHKNIGHT" then
-		RuneFrame:Show()
+	if not legion_700 then
+		unhook_frames(WarlockPowerFrame, TotemFrame, EclipseBarFrame, PaladinPowerBar, RuneFrame, PriestBarFrame, MonkHarmonyBar)
+
+		local _, class = UnitClass("player")
+		if class == "WARLOCK" then
+			WarlockPowerFrame_SetUpCurrentPower()
+		elseif class == "SHAMAN" then
+			TotemFrame_Update()
+		elseif class == "DRUID" then
+			EclipseBar_UpdateShown(EclipseBarFrame)
+		elseif class == "PALADIN" then
+			PaladinPowerBar:Show()
+			PaladinPowerBar_Update(PaladinPowerBar)
+		elseif class == "DEATHKNIGHT" then
+			RuneFrame:Show()
+			RuneFrame:GetScript("OnEvent")(RuneFrame, "PLAYER_ENTERING_WORLD")
+		elseif class == "PRIEST" then
+			PriestBarFrame_CheckAndShow()
+		elseif class == "MONK" then
+			MonkHarmonyBar:Show()
+			MonkHarmonyBar_Update(MonkHarmonyBar)
+		end
+	else
+		unhook_frames(PlayerFrame.classPowerBar, TotemFrame, EclipseBarFrame, RuneFrame, PriestBarFrame)
+
+		local _, class = UnitClass("player")
+		if PlayerFrame.classPowerBar then
+			PlayerFrame.classPowerBar:Setup()
+		elseif class == "SHAMAN" then
+			TotemFrame_Update()
+		elseif class == "DRUID" then
+			EclipseBar_UpdateShown(EclipseBarFrame)
+		elseif class == "DEATHKNIGHT" then
+			RuneFrame:Show()
+			RuneFrame:GetScript("OnEvent")(RuneFrame, "PLAYER_ENTERING_WORLD")
+		elseif class == "PRIEST" then
+			PriestBarFrame_CheckAndShow()
+		end
 	end
-	RuneFrame:GetScript("OnLoad")(RuneFrame)
-	RuneFrame:GetScript("OnEvent")(RuneFrame, "PLAYER_ENTERING_WORLD")
 end
 
 function hiders:aura()
-	BuffFrame:Hide()
-	TemporaryEnchantFrame:Hide()
-	ConsolidatedBuffs:Hide()
-	BuffFrame:UnregisterAllEvents()
+	hook_frames(BuffFrame, TemporaryEnchantFrame)
+	if not legion_700 then
+		hook_frames(ConsolidatedBuffs)
+	end
 end
 
 function showers:aura()
+	unhook_frames_without_init(BuffFrame, TemporaryEnchantFrame)
+	BuffFrame:RegisterUnitEvent("UNIT_AURA", "player", "vehicle")
+	BuffFrame:RegisterEvent("GROUP_ROSTER_UPDATE")
+	BuffFrame:RegisterEvent("PLAYER_SPECIALIZATION_CHANGED")
 	BuffFrame:Show()
-	if GetCVarBool("consolidateBuffs") then
-		ConsolidatedBuffs:Show()
-	end
+
 	TemporaryEnchantFrame:Show()
 
-	-- Can't use OnLoad because doing so resets some variables which
-	-- requires an update to get the frame back in the proper state,
-	-- which in Cata causes taint.
-	BuffFrame:RegisterEvent("UNIT_AURA")
-
-	-- This isn't perfect.  It doesn't update the buffs till the next
-	-- aura update.  However, in Cata it causes taint to force the update.
-	-- However, it should work for 99% of peoples use cases, which is toggling
-	-- it on and off to see what it does or setting it and leaving it set.
+	if not legion_700 then
+		unhook_frames_without_init(ConsolidatedBuffs)
+		if ShouldShowConsolidatedBuffFrame() then
+			ConsolidatedBuffs:Show()
+		end
+	end
 end
 
 function hiders:altpower()
-	PlayerPowerBarAlt:UnregisterAllEvents()
-	PlayerPowerBarAlt:Hide()
+	hook_frames(PlayerPowerBarAlt)
 end
 
 function showers:altpower()
-	PlayerPowerBarAlt:GetScript("OnLoad")(PlayerPowerBarAlt)
+	unhook_frames_without_init(PlayerPowerBarAlt)
+	PlayerPowerBarAlt:RegisterEvent("UNIT_POWER_BAR_SHOW")
+	PlayerPowerBarAlt:RegisterEvent("UNIT_POWER_BAR_HIDE")
+	PlayerPowerBarAlt:RegisterEvent("PLAYER_ENTERING_WORLD")
 	UnitPowerBarAlt_UpdateAll(PlayerPowerBarAlt)
 end
 
 function hiders:boss()
 	for i=1, MAX_BOSS_FRAMES do
 		local frame = _G["Boss"..i.."TargetFrame"]
-		frame:UnregisterAllEvents()
-		frame:Hide()
+		hook_frames(frame)
 	end
 end
 
 function showers:boss()
 	for i=1, MAX_BOSS_FRAMES do
 		local frame = _G["Boss"..i.."TargetFrame"]
+		unhook_frames_without_init(frame)
 		if i == 1 then
 			BossTargetFrame_OnLoad(frame, "boss1", "INSTANCE_ENCOUNTER_ENGAGE_UNIT")
 		else
@@ -265,7 +343,7 @@ PitBull4_HideBlizzard:SetGlobalOptionsFunction(function(self)
 	local function set(info, value)
 		local id = info[#info]
 		self.db.profile.global[id] = value
-		
+
 		self:UpdateFrames()
 	end
 	local function hidden(info)
@@ -277,21 +355,22 @@ PitBull4_HideBlizzard:SetGlobalOptionsFunction(function(self)
 		desc = L["Hide the standard player frame."],
 		get = get,
 		set = set,
-		hidden = hidden,	
+		hidden = hidden,
+	}, 'runebar', {
+		type = 'toggle',
+		name = "Class power bar",
+		desc = "Hides the class resource bar attached to your player frame.",
+		get = get,
+		set = set,
+		hidden = hidden,
+		disabled = function() return self.db.profile.global.player	end,
 	}, 'party', {
 		type = 'toggle',
 		name = L["Party"],
 		desc = L["Hide the standard party frames."],
-		get = function (info)
-			if GetDisplayedAllyFrames() == "raid" then
-				return false
-			else
-				return get(info)
-			end
-		end,
+		get = get,
 		set = set,
-		hidden = hidden,	
-		disabled = function() return GetDisplayedAllyFrames() == "raid" end,
+		hidden = hidden,
 	}, 'raid', {
 		type = 'toggle',
 		name = L["Raid"],
@@ -305,35 +384,28 @@ PitBull4_HideBlizzard:SetGlobalOptionsFunction(function(self)
 		desc = L["Hide the standard target frame."],
 		get = get,
 		set = set,
-		hidden = hidden,	
+		hidden = hidden,
 	}, 'focus', {
 		type = 'toggle',
 		name = L["Focus"],
 		desc = L["Hide the standard focus frame."],
 		get = get,
 		set = set,
-		hidden = hidden,	
+		hidden = hidden,
 	}, 'castbar', {
 		type = 'toggle',
 		name = L["Cast bar"],
 		desc = L["Hides the standard cast bar."],
 		get = get,
 		set = set,
-		hidden = hidden,	
+		hidden = hidden,
 	}, 'aura', {
 		type = 'toggle',
 		name = L["Buffs/debuffs"],
 		desc = L["Hides the standard buff/debuff frame in the top-right corner of the screen."],
 		get = get,
 		set = set,
-		hidden = hidden,	
-	}, 'runebar', {
-		type = 'toggle',
-		name = L["Rune bar"],
-		desc = L["Hides the standard rune bar in the top-left corner of the screen."],
-		get = get,
-		set = set,
-		hidden = hidden,	
+		hidden = hidden,
 	}, 'altpower', {
 		type = 'toggle',
 		name = L["Alternate power"],
