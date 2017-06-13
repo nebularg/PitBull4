921082646d6bb2e98d60a33df50a90aa71850f3a
Kyle Buller <bullerk@gmail.com>
2017-06-13 08:56:18 -0500

    Rework PvPIcon to show Prestige rank by default.
    Closes #1197

--- Modules/PvPIcon/PvPIcon.lua
+++ Modules/PvPIcon/PvPIcon.lua
@@ -11,32 +11,10 @@ PitBull4_PvPIcon:SetDefaults({
 	attach_to = "root",
 	location = "edge_top_right",
 	position = 1,
+	show_prestige = true,
 })
 
-function PitBull4_PvPIcon:OnEnable()
-	self:RegisterEvent("UPDATE_FACTION")
-	self:RegisterEvent("PLAYER_FLAGS_CHANGED", "UPDATE_FACTION")
-	self:RegisterEvent("UNIT_FACTION", "UPDATE_FACTION")
-end
-
-function PitBull4_PvPIcon:GetTexture(frame)
-	local unit = frame.unit
-
-	if UnitIsPVPFreeForAll(unit) then
-		return [[Interface\TargetingFrame\UI-PVP-FFA]]
-	end
-
-	local faction = UnitFactionGroup(unit) or UnitFactionGroup("player")
-	if not faction or faction == "Neutral" then
-		return nil
-	end
-
-	if not UnitIsPVP(unit) then
-		return nil
-	end
-
-	return [[Interface\TargetingFrame\UI-PVP-]] .. faction
-end
+local INDICATOR_SIZE = 15
 
 local FRIENDLY_CLASSIFICATIONS = {
 	player = true,
@@ -48,10 +26,56 @@ local FRIENDLY_CLASSIFICATIONS = {
 	targettarget = true,
 	bosstarget = true,
 }
+
 local OPPOSITE_PLAYER_FACTION = {
 	["Horde"] = "Alliance",
 	["Alliance"] = "Horde",
+	["FFA"] = "FFA",
 }
+
+local TEX_COORDS = {
+	[ [[Interface\TargetingFrame\UI-PVP-FFA]] ] = {0.05, 0.605, 0.015, 0.57},
+	[ [[Interface\TargetingFrame\UI-PVP-Horde]] ] = {0.08, 0.58, 0.045, 0.545},
+	[ [[Interface\TargetingFrame\UI-PVP-Alliance]] ] = {0.07, 0.58, 0.06, 0.57},
+}
+
+local PRESTIGE_TEX_COORDS = {
+	[ [[Interface\TargetingFrame\UI-PVP-FFA]] ] = {0.0517578, 0.100586, 0.763672, 0.865234}, -- honorsystem-portrait-neutral
+	[ [[Interface\TargetingFrame\UI-PVP-Horde]] ] = {0.000976562, 0.0498047, 0.869141, 0.970703}, -- honorsystem-portrait-horde
+	[ [[Interface\TargetingFrame\UI-PVP-Alliance]] ] = {0.000976562, 0.0498047, 0.763672, 0.865234}, -- honorsystem-portrait-alliance
+}
+
+function PitBull4_PvPIcon:OnEnable()
+	self:RegisterEvent("UPDATE_FACTION")
+	self:RegisterEvent("PLAYER_FLAGS_CHANGED", "UPDATE_FACTION")
+	self:RegisterEvent("UNIT_FACTION", "UPDATE_FACTION")
+	self:RegisterEvent("HONOR_PRESTIGE_UPDATE", "UPDATE_FACTION")
+end
+
+function PitBull4_PvPIcon:GetTexture(frame)
+	local unit = frame.unit
+
+	if UnitIsPVPFreeForAll(unit) then
+		return [[Interface\TargetingFrame\UI-PVP-FFA]]
+	end
+
+	local faction = UnitFactionGroup(unit)
+	if not faction or faction == "Neutral" then
+		return nil
+	end
+
+	if not UnitIsPVP(unit) then
+		return nil
+	end
+
+	-- Handle "Mercenary Mode" added in 6.2.2. This is only used for PlayerFrame
+	-- so I'm assumming other units don't require this check.
+	if unit == "player" and UnitIsMercenary(unit) then
+		faction = OPPOSITE_PLAYER_FACTION[faction]
+	end
+	return [[Interface\TargetingFrame\UI-PVP-]] .. faction
+end
+
 function PitBull4_PvPIcon:GetExampleTexture(frame)
 	local classification = frame.is_singleton and frame.unit or frame.header.unit_group
 	if classification == "focus" then
@@ -59,11 +83,8 @@ function PitBull4_PvPIcon:GetExampleTexture(frame)
 	end
 
 	local player_faction = UnitFactionGroup("player")
-	-- Pandarens start out with a Neutral faction, so just make config mode
-	-- guess that Pandarens are all going to be Alliance so we have something
-	-- to show.
 	if not player_faction or player_faction == "Neutral" then
-		player_faction = "Alliance"
+		player_faction = "FFA"
 	end
 
 	if FRIENDLY_CLASSIFICATIONS[classification] or (frame.guid and frame.unit and UnitIsFriend("player", frame.unit)) then
@@ -71,18 +92,6 @@ function PitBull4_PvPIcon:GetExampleTexture(frame)
 	end
 	return [[Interface\TargetingFrame\UI-PVP-]] .. OPPOSITE_PLAYER_FACTION[player_faction]
 end
-
-local tex_coords = {
-	[ [[Interface\TargetingFrame\UI-PVP-FFA]] ] = {0.05, 0.605, 0.015, 0.57},
-	[ [[Interface\TargetingFrame\UI-PVP-Horde]] ] = {0.08, 0.58, 0.045, 0.545},
-	[ [[Interface\TargetingFrame\UI-PVP-Alliance]] ] = {0.07, 0.58, 0.06, 0.57},
-}
-
-function PitBull4_PvPIcon:GetTexCoord(frame, texture)
-	local tex_coord = tex_coords[texture]
-	return tex_coord[1], tex_coord[2], tex_coord[3], tex_coord[4]
-end
-PitBull4_PvPIcon.GetExampleTexCoord = PitBull4_PvPIcon.GetTexCoord
 
 function PitBull4_PvPIcon:UPDATE_FACTION(event, unit)
 	if not unit then
@@ -94,3 +103,95 @@ function PitBull4_PvPIcon:UPDATE_FACTION(event, unit)
 		self:UpdateForUnitID(unit_pet)
 	end
 end
+
+function PitBull4_PvPIcon:ClearFrame(frame)
+	local container = frame.PvPIcon
+	if not container then
+		return false
+	end
+
+	container.PrestigePortrait = container.PrestigePortrait:Delete()
+	container.PrestigeBadge = container.PrestigeBadge:Delete()
+	container.PrestigeIcon = container.PrestigeIcon:Delete()
+	container.PvPIcon = container.PvPIcon:Delete()
+	frame.PvPIcon = container:Delete()
+
+	return true
+end
+
+function PitBull4_PvPIcon:UpdateFrame(frame)
+	local texture
+	if frame.guid and frame.unit then
+		texture = self:GetTexture(frame)
+	end
+	if not texture and frame.force_show then
+		texture = self:GetExampleTexture(frame)
+	end
+
+	if not texture then
+		return self:ClearFrame(frame)
+	end
+
+	local container = frame.PvPIcon
+	local made_control = not container
+	if made_control then
+		container = PitBull4.Controls.MakeFrame(frame)
+		container:SetFrameLevel(frame:GetFrameLevel() + 13)
+		container:SetSize(INDICATOR_SIZE, INDICATOR_SIZE)
+		frame.PvPIcon = container
+
+		local PvPIcon = PitBull4.Controls.MakeTexture(container, "ARTWORK")
+		PvPIcon:SetAllPoints(container)
+		container.PvPIcon = PvPIcon
+
+		local PrestigeIcon = PitBull4.Controls.MakeFrame(container)
+		PrestigeIcon:SetPoint("CENTER")
+		PrestigeIcon:SetSize(30, 30)
+		PrestigeIcon:SetScale(0.5)
+		container.PrestigeIcon = PrestigeIcon
+
+		local PrestigePortrait = PitBull4.Controls.MakeTexture(PrestigeIcon, "ARTWORK")
+		PrestigePortrait:SetPoint("CENTER")
+		PrestigePortrait:SetSize(50, 52)
+		PrestigePortrait:SetTexture([[Interface\PVPFrame\PvPPrestigeIcons]])
+		container.PrestigePortrait = PrestigePortrait
+
+		local PrestigeBadge = PitBull4.Controls.MakeTexture(PrestigeIcon, "ARTWORK", 1)
+		PrestigeBadge:SetPoint("CENTER")
+		PrestigeBadge:SetSize(30, 30)
+		container.PrestigeBadge = PrestigeBadge
+	end
+
+	local prestige = UnitPrestige(frame.unit)
+	if prestige > 0 and self:GetLayoutDB(frame).show_prestige then
+		container.PrestigePortrait:SetTexCoord(unpack(PRESTIGE_TEX_COORDS[texture]))
+		container.PrestigeBadge:SetTexture(GetPrestigeInfo(prestige))
+		container.PrestigeIcon:Show()
+		container.PvPIcon:Hide()
+	else
+		container.PrestigeIcon:Hide()
+		container.PvPIcon:SetTexture(texture)
+		container.PvPIcon:SetTexCoord(unpack(TEX_COORDS[texture]))
+		container.PvPIcon:Show()
+	end
+	container:Show()
+
+	return made_control
+end
+
+PitBull4_PvPIcon:SetLayoutOptionsFunction(function(self)
+	local function get(info)
+		return PitBull4.Options.GetLayoutDB(self)[info[#info]]
+	end
+	local function set(info, value)
+		PitBull4.Options.GetLayoutDB(self)[info[#info]] = value
+		PitBull4.Options.UpdateFrames()
+	end
+
+	return "show_prestige", {
+		type = "toggle",
+		name = L["Show prestige rank"],
+		get = get,
+		set = set,
+	}
+end)
