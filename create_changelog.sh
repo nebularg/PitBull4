527b73e1a50679912f38dad49e3f7885709a447d
Kyle Buller <bullerk@gmail.com>
2019-10-23 19:22:13 -0500

    [ci] Fix CHANGELOG for alpha builds

--- create_changelog.sh
+++ create_changelog.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+
 current=$( git describe --tags --always )
-previous="${current%?}1"
-if [ "$current" = "$previous" ]; then
-	previous=$( git describe --tags --abbrev=0 --match="*classic*" HEAD~ )
+previous=$( git describe --tags --always --abbrev=0 )
+if [ "$current" = "$previous" ]; then # on tag
+	previous="${current%?}1"
+	if [ "$current" = "$previous" ]; then # first tag in the series
+		previous=$( git describe --tags --abbrev=0 --match="*classic*" HEAD~ )
+	fi
 fi
 
 date=$( git log "$current" -1 --date=short --format="%ad" )
@@ -16,10 +20,10 @@ cat << EOF > "CHANGELOG.md"
 [Full Changelog](${repo_url}/compare/${previous}...${current})
 
 EOF
-git log "$previous..$current" --pretty=format:"###%B" \
+git log "$previous..$current" --grep="^\[ci\]" --invert-grep --pretty=format:"###%B" \
 	| sed -e 's/^/    /g' -e 's/^ *$//g' -e 's/^    ###/- /g' -e 's/$/  /' \
 	      -e 's/\([a-zA-Z0-9]\)_\([a-zA-Z0-9]\)/\1\\_\2/g' \
-	      -e 's/\[ci skip\]//g' -e 's/\[skip ci\]//g' \
-	      -e '/git-svn-id:/d' -e '/^\s*This reverts commit [0-9a-f]\{40\}\.\s*$/d' \
+	      -e 's/\[ci skip\]//g' \
+	      -e '/^\s*This reverts commit [0-9a-f]\{40\}\.\s*$/d' \
 	      -e '/^\s*$/d' \
 	>> "CHANGELOG.md"
