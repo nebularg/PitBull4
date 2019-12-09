75805bcf379b6000f7f68985ea1c5691527fef00
Kyle Buller <bullerk@gmail.com>
2019-12-08 19:35:51 -0600

    ArtifactPowerBar: Hide the bar at max level

--- Modules/ArtifactPowerBar/ArtifactPowerBar.lua
+++ Modules/ArtifactPowerBar/ArtifactPowerBar.lua
@@ -14,38 +14,14 @@ PitBull4_ArtifactPowerBar:SetDefaults({
 	position = 8,
 })
 
-local function GetArtifactXP()
-	local azeriteItemLocation = C_AzeriteItem.FindActiveAzeriteItem()
-	if azeriteItemLocation then
-		-- default UI shows an empty bar if the azerite item is in your bank (api can error)
-		-- if azeriteItemLocation.bagID and (azeriteItemLocation.bagID < 0 or azeriteItemLocation.bagID > NUM_BAG_SLOTS) then
-		-- 	return 0, 1
-		-- end
-
-		-- hide if not equipped
-		if not azeriteItemLocation:IsEquipmentSlot() then
-			return
-		end
-
-		return C_AzeriteItem.GetAzeriteItemXPInfo(azeriteItemLocation)
-	end
-end
-
 function PitBull4_ArtifactPowerBar:OnEnable()
 	self:RegisterEvent("AZERITE_ITEM_EXPERIENCE_CHANGED")
 	self:RegisterEvent("PLAYER_ENTERING_WORLD", "AZERITE_ITEM_EXPERIENCE_CHANGED")
-	-- self:RegisterEvent("BAG_UPDATE_DELAYED", "AZERITE_ITEM_EXPERIENCE_CHANGED")
-	self:RegisterEvent("PLAYER_EQUIPMENT_CHANGED") -- handle (un)equip
+	self:RegisterEvent("PLAYER_EQUIPMENT_CHANGED", "AZERITE_ITEM_EXPERIENCE_CHANGED")
 end
 
 function PitBull4_ArtifactPowerBar:AZERITE_ITEM_EXPERIENCE_CHANGED()
 	self:UpdateForUnitID("player")
-end
-
-function PitBull4_ArtifactPowerBar:PLAYER_EQUIPMENT_CHANGED(_, slot)
-	if slot == 2 then -- neck
-		self:UpdateForUnitID("player")
-	end
 end
 
 function PitBull4_ArtifactPowerBar:GetValue(frame)
@@ -53,7 +29,20 @@ function PitBull4_ArtifactPowerBar:GetValue(frame)
 		return
 	end
 
-	local value, max = GetArtifactXP()
+	if not C_AzeriteItem.HasActiveAzeriteItem() then
+		return
+	end
+
+	if C_AzeriteItem.IsAzeriteItemAtMaxLevel() then
+		return
+	end
+
+	local azeriteItemLocation = C_AzeriteItem.FindActiveAzeriteItem()
+	if not azeriteItemLocation or not azeriteItemLocation:IsEquipmentSlot() then
+		return
+	end
+
+	local value, max = C_AzeriteItem.GetAzeriteItemXPInfo(azeriteItemLocation)
 	if value then
 		return value / max
 	end
