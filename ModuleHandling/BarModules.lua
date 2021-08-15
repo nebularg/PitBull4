b712eeb77d19432a594381e9fb41c4fc64480465
Kyle Buller <bullerk@gmail.com>
2021-08-15 12:27:22 -0500

    Fix an error when using a LSM statusbar that is a file id
    Fixes #27

--- ModuleHandling/BarModules.lua
+++ ModuleHandling/BarModules.lua
@@ -466,7 +466,9 @@ function BarModule:GetTexture(frame)
 	local texture
 	if LibSharedMedia then
 		texture = LibSharedMedia:Fetch("statusbar", layout_db.texture or frame.layout_db.bar_texture or "Blizzard")
-		texture = texture:gsub("%.tga$", ""):gsub("%.blp$", "")
+		if type(texture) == "string" then
+			texture = texture:gsub("%.tga$", ""):gsub("%.blp$", "")
+		end
 	end
 	return texture or [[Interface\TargetingFrame\UI-StatusBar]]
 end
