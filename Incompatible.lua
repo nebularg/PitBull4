a6c3f292f9a28dff17c5f435ec95d1d10d8a47c1
Kyle Buller <bullerk@gmail.com>
2022-02-26 13:21:06 -0600

    Update TOC for 9.2 and only load for retail

---
+++ Incompatible.lua
@@ -0,0 +1,7 @@
+
+C_Timer.After(2, function()
+	print("|cff33ff99PitBull4|r: You are using the wrong version of PitBull4 for retail!")
+	print("|cff33ff99PitBull4|r: Please install the correct classic version.")
+	BasicMessageDialog.Text:SetText("You are using the wrong version of PitBull4 for retail!")
+	BasicMessageDialog:Show()
+end)
