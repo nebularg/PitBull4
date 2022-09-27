98e7b2b53ac41408276f9619db324678262d910f
Kyle Buller <bullerk@gmail.com>
2022-09-26 22:55:48 -0500

    Remove fallback toc

--- Incompatible.lua
+++
@@ -1,7 +0,0 @@
-
-C_Timer.After(2, function()
-	print("|cff33ff99PitBull4|r: You are using the wrong version of PitBull4 for classic!")
-	print("|cff33ff99PitBull4|r: Please install the correct Retail version.")
-	BasicMessageDialog.Text:SetText("You are using the wrong version of PitBull4 for classic!")
-	BasicMessageDialog:Show()
-end)
